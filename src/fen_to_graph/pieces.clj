7afd530cc03e9a0afa8f7b1da528a9437c01157b
Marco Dalla Stella <marco@dallastella.name>
2015-06-16 23:33:55 +0200

    Documentation of pieces.clj fixed.

--- src/fen_to_graph/pieces.clj
+++ src/fen_to_graph/pieces.clj
@@ -1,33 +1,39 @@
 (ns fen-to-graph.pieces
   (:require [fen-to-graph.board :as board]))
 
-(defn- position-to-int [position]
+(defn- position-to-int
   "Takes a position, split it in characters and return a list of
   int of it. Es. :e2 -> (101 50)."
+  [position]
   (map int (name position)))
 
-(defn- int-to-position [file rank]
+(defn- int-to-position
   "Takes two ints that represent the file and the rank characters of a
   position and return a keyword of it. Es. 101,50 -> :e2"
+  [file rank]
   (keyword (str (char file) (char rank))))
 
-(defn up [position]
+(defn up
   "Move the given position up. Es. :e2 -> :e3"
+  [position]
   (let [[file rank] (position-to-int position)]
     (int-to-position file (inc rank))))
 
-(defn down [position]
+(defn down
   "Move the given position down. Es. :e2 -> :e1"
+  [position]
   (let [[file rank] (position-to-int position)]
     (int-to-position file (dec rank))))
 
-(defn left [position]
-  "Move the given position left. Es. :e2 -> :d2"
+(defn left
+  "Move the given position to left. Es. :e2 -> :d2"
+  [position]
   (let [[file rank] (position-to-int position)]
     (int-to-position (dec file) rank)))
 
-(defn right [position]
-  "Move the given position right. Es. :e2 -> :f2"
+(defn right
+  "Move the given position to the right. Es. :e2 -> :f2"
+  [position]
   (let [[file rank] (position-to-int position)]
     (int-to-position (inc file) rank)))
 
@@ -44,9 +50,10 @@
   (let [initial-position {:white \2 :black \7}]
     (= (second (name position)) (initial-position color))))
 
-(defn- pawn-moves [color position]
+(defn- pawn-moves
   "Returns a list of functions that calculate the possible moves of a
   pawn."
+  [color position]
   (if (= color :white)
     (let [moves [up-left up up-right]]
       (if (pawn-initial? color position)
@@ -57,9 +64,10 @@
         (conj moves (comp down down))
         moves))))
 
-(defn- move-until-valid [direction position board]
+(defn- move-until-valid
   "Return a list of valid positions given a direction. It stops when a
   position is out of the board or occupied by another piece."
+  [direction position board]
   (loop [next-pos (direction position)
          moves []]
     (cond
@@ -68,8 +76,9 @@
       :else (recur (direction next-pos)
                    (conj moves next-pos)))))
 
-(defn- bishop-moves [position board]
+(defn- bishop-moves
   "Returns all the possible positions of a bishop."
+  [position board]
   (loop [functions [up-left up-right down-left down-right]
          moves []]
     (if (empty? functions)
@@ -78,8 +87,9 @@
              (conj moves (move-until-valid (first functions)
                                            position
                                            board))))))
-(defn- rook-moves [position board]
+(defn- rook-moves
   "Returns all the possible positions of a rook."
+  [position board]
   (loop [functions [up right down left]
          moves []]
     (if (empty? functions)
@@ -89,8 +99,9 @@
                                            position
                                            board))))))
 
-(defn- queen-moves [position board]
+(defn- queen-moves
   "Returns all the possible positions of a queen."
+  [position board]
   (concat
    (bishop-moves position board)
    (rook-moves position board)))
