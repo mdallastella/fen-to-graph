c4ad1730c71282bf49f7bd7d420a4e91c57e2749
Marco Dalla Stella <marco@dallastella.name>
2015-06-17 16:40:08 +0200

    Added codox for documentation.

--- project.clj
+++ project.clj
@@ -5,12 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [aysylu/loom "0.5.0"]
-                 [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]
-  :plugins [[lein-environ "1.0.0"]]
+                 [aysylu/loom "0.5.0"]]
+  :plugins [[lein-environ "1.0.0"]
+            [codox "0.8.12"]]
   :main ^:skip-aot fen-to-graph.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[midje "1.6.3"]]
-                   :env {:squiggly {:checkers [:eastwood]
-                                    :eastwood-exclude-linters [:unlimited-use]}}}})
+  :profiles {:uberjar {:aot :all}})
